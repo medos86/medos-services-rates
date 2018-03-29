2228ce8343b79aa941b3a2272ca0b5b9232026f5
mbs <mohamed.besbes@edifixio.com>
2018-03-29 15:47:20 +0200

    [FEATURE] maven

---
+++ src/test/java/com/medos/rates/medossrvicesrates/MedosSrvicesRatesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medos.rates.medossrvicesrates;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedosSrvicesRatesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
